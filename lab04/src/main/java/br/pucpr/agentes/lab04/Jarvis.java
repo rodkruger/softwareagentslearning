7832081666886ff0799e7cc926cc3817aa82efd4
Rodrigo Krüger <rodrigo.kruger.eng@gmail.com>
2020-06-06 09:57:16 -0300

    Ajustes Aula 04

--- lab04/src/main/java/br/pucpr/agentes/lab04/Jarvis.java
+++ lab04/src/main/java/br/pucpr/agentes/lab04/Jarvis.java
@@ -10,7 +10,23 @@ public class Jarvis extends Agent {
 
     @Override
     protected void setup() {
-        log("Olá, eu sou o agente " + getName());
+        try {
+            log("Olá, eu sou o agente " + getName());
+            log("Pode me chamar apenas de " + getLocalName());
+            log("HAP: " + getHap());
+            log("AgentState: " + getAgentState());
+            log("AID: " + getAID());
+            log("AMS: " + getAMS());
+            log("ContainerController.name: " + getContainerController().getName());
+            log("ContainerController.containerName: " + getContainerController().getContainerName());
+            log("ContainerController.platformName: " + getContainerController().getPlatformName());
+            log("ContentManager: " + getContentManager());
+            log("O2AObject " + getO2AObject());
+            log("QueueSize: " + getQueueSize());
+            log("State: " + getState());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
